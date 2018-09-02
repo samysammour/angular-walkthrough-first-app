cf614aa3b64d698f87efe8f47072ed7c3a2e3803
ss.net <samy.sammour@hotmail.com>
2018-09-02 10:17:19 +0200

    Add course service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerModule } from './customer/customer.module';
+import { AppRoutingModule } from './app.routing';
+import { ErrorNotFoundComponent } from './shared/error-not-found/error-not-found.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { CourseModule } from './course/course.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorNotFoundComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomerModule,
+    AppRoutingModule,
+    CourseModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+],
 })
 export class AppModule { }
